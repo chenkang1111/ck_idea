9f2d5c9789ea82c37ff106b82a6faa837cecc987
chenkang1111 <767818668@qq.com>
2019-11-08 11:47:03 +0800

    本地仓库接受

---
+++ ssm/src/com/chenkang/Demo1.java
@@ -0,0 +1,13 @@
+package com.chenkang;
+
+/**
+ * @author陈康
+ * @site www.chenkang.com
+ * @company xxx公司
+ * @create 2019-11-08 11:42
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("idea git demo");
+    }
+}
